ebac046b69440476c8953d5e6e4ce248f93f62a7
Ritik Rawat <rithikrawat2004@gmail.com>
2025-06-26 16:27:33 +0530

    API Routes

--- routes/incomeRoutes.js
+++ routes/incomeRoutes.js
@@ -3,6 +3,7 @@ const {
     addIncome,
     getAllIncome,
     deleteIncome,
+    updateIncome,
     downloadIncomeExcel
 } = require('../controllers/incomeController.js');
 const {protect} = require('../middleware/authMiddleware.js');
@@ -12,6 +13,7 @@ const router = express.Router();
 router.post('/add', protect, addIncome);
 router.get('/get', protect, getAllIncome);
 router.get('/downloadexcel', protect, downloadIncomeExcel);
+router.put('/:id', protect, updateIncome);
 router.delete('/:id', protect, deleteIncome);
 
 

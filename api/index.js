cf05c566a22e8cf2e67b5880f38da0005e5b1727
Ritik Rawat <rithikrawat2004@gmail.com>
2025-06-27 18:10:48 +0530

    Server Functions

--- api/index.js
+++ api/index.js
@@ -12,7 +12,6 @@ const authRoutes = require('../routes/authRoutes');
 const incomeRoutes = require('../routes/incomeRoutes');
 const expenseRoutes = require('../routes/expenseRoutes');
 const dashboardRoutes = require('../routes/dashboardRoutes');
-const validateEmail = require('../routes/validateEmail')
 
 const app = express();
 
@@ -37,7 +36,6 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/income', incomeRoutes);
 app.use('/api/v1/expense', expenseRoutes);
 app.use('/api/v1/dashboard', dashboardRoutes);
-app.use('/api/v1/validate-email', validateEmail);
 
 // Serve uploaded images
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));

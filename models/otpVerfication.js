49dff527e9a520ceaf7bc308a072f8a41493c3bf
Ritik Rawat <rithikrawat2004@gmail.com>
2025-06-28 16:04:07 +0530

    Data Schemas

---
+++ models/otpVerfication.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const otpSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  expiresAt: { 
+    type: Date, 
+    required: true,
+    index: { expires: 0 }
+  },
+});
+
+module.exports = mongoose.model('OtpVerification', otpSchema);
